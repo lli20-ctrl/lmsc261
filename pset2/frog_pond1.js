5c2f24f6feb31740e1405569371a8ac6307007d6
2070544190-coder <chuqingqing988@gmail.com>
2026-09-25 18:23:19 -0400

    frog

---
+++ pset2/frog_pond1.js
@@ -0,0 +1,2 @@
+let amountOfFrog = prompt("How many frogs are about to jump in?")
+amountOfFrog <= 15 ? print("Come on in!") : print("It’s too crowded!");
---
+++ pset2/frog_pond2.js
@@ -0,0 +1,5 @@
+const activities = ["babysit tadpoles", "flies for lunch", "tongue stretch", "swimming lesson"];	
+let userInput = prompt("Which activity do you want?")
+
+userInput <= 3 && userInput >= 0 ? print(activities[userInput]) : print(activities[0]);
+
